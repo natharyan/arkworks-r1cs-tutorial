2c84e1dda967f2fe370ab14f72f81150080c23b8
natharyan <aryannath2004@gmail.com>
2025-11-12 20:04:56 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use ark_r1cs_std::{alloc::AllocVar, fields::fp::FpVar, eq::EqGadget};
 use ark_ff::{PrimeField};
 use ark_bls12_381::Fr;
 use std::env;
+use ark_groth16::{Groth16, ProvingKey, VerifyingKey, Proof};
+use ark_snark::SNARK;
+use ark_bls12_381::Bls12_381;
+use ark_std::rand::thread_rng;
+use std::time::Instant;
 
 #[derive(Clone, Debug)]
 pub struct CubeCircuit<F: PrimeField> {
@@ -34,6 +39,7 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for CubeCircuit<F> {
 fn main() {
     unsafe { env::set_var("RUST_BACKTRACE", "1"); }
     let x_value = Fr::from(3u64);
+    // y = x^3 + x + 5
     // 3^3 + 3 + 5 = 9*3 + 3 + 5 = 35
     let y_value = x_value * x_value * x_value + x_value + Fr::from(5u64);
 
@@ -43,8 +49,7 @@ fn main() {
     };
 
     let cs = ConstraintSystem::new_ref();
-    // cs.set_mode(SynthesisMode::Setup); // stores symbolic representations of constraints to enable use of to_matrices()
-    circuit.generate_constraints(cs.clone()).unwrap();
+    circuit.clone().generate_constraints(cs.clone()).unwrap();
     assert!(cs.is_satisfied().unwrap());
     println!("Equation: y = x^3 + x + 5");
     println!("Private input x: {}", x_value);
@@ -57,11 +62,16 @@ fn main() {
     z.extend(cs.borrow().unwrap().instance_assignment.clone());
     z.extend(cs.borrow().unwrap().witness_assignment.clone());
 
-    println!("\nVector z: {:?}", z);
+    // get the integer values of z from BigInt representation
+
+    let z_str = z.iter().map(|val| val.to_string()).collect::<Vec<String>>();
+    println!("\nVector z: [{}]", z_str.join(", "));
 
     let (instance, witness) = z.split_at(cs.borrow().unwrap().instance_assignment.len());
-    println!("Instance variables: {:?}", instance);
-    println!("Witness variables: {:?}", witness);
+    let instance_str = instance.iter().map(|val| val.to_string()).collect::<Vec<String>>();
+    let witness_str = witness.iter().map(|val| val.to_string()).collect::<Vec<String>>();
+    println!("\nInstance variables: [{}]", instance_str.join(", "));
+    println!("Witness variables: [{}]", witness_str.join(", "));
 
     cs.inline_all_lcs();
     let matrices: ConstraintMatrices<Fr> = cs.borrow().unwrap().to_matrices().unwrap();
@@ -69,34 +79,58 @@ fn main() {
     let a: Vec<Vec<(Fr, usize)>> = matrices.a;
     let b: Vec<Vec<(Fr, usize)>> = matrices.b;
     let c: Vec<Vec<(Fr, usize)>> = matrices.c;
+    
+    // convert to BigInt to string. BigInt: x = a0​+  (a_1​ × 2^64) + (a_2 ​× 2^128) + (a_3 ​× 2^192)
+    let a_str: Vec<Vec<(String, usize)>> = a.iter().map(|row| row.iter().map(|(val, idx)| (val.to_string(), *idx)).collect()).collect();
+    let b_str: Vec<Vec<(String, usize)>> = b.iter().map(|row| row.iter().map(|(val, idx)| (val.to_string(), *idx)).collect()).collect();
+    let c_str: Vec<Vec<(String, usize)>> = c.iter().map(|row| row.iter().map(|(val, idx)| (val.to_string(), *idx)).collect()).collect();
+
 
     println!("\nMatrix A:");
-    for row in a.iter() {
-        println!("{:?}", row); 
-    } // 52435875175126190479447740508185965837690552500527637822603658699938581184512 \equiv -1 mod p
+    for row in a_str.iter() {
+        println!("[{}]", row.iter().map(|(val, idx)| format!("({}, {})", val, idx)).collect::<Vec<_>>().join(", "));
+    }
     println!("\nMatrix B:");
-    for row in b.iter() {
-        println!("{:?}", row);
+    for row in b_str.iter() {
+        println!("[{}]", row.iter().map(|(val, idx)| format!("({}, {})", val, idx)).collect::<Vec<_>>().join(", "));
     }
-
     println!("\nMatrix C:");
-    for row in c.iter() {
-        println!("{:?}", row);
+    for row in c_str.iter() {
+        println!("[{}]", row.iter().map(|(val, idx)| format!("({}, {})", val, idx)).collect::<Vec<_>>().join(", "));
     }
 
-    // Enforces x^2 = x * x
+    // z = [1, 35, 3, 9, 27] = [1] + [public input] + [private witnesses]
+    // each constraint adds a row to the matrices
+    // enforces x^2 = x * x
     // A: [(1, 2)] => 1*z[2] = 1*3 = 3
     // B: [(1, 2)] => 1*z[2] = 1*3 = 3
     // C: [(1, 3)] => 1*z[3] = 1*9 = 9
 
-    // Enforces x^3 = x^2 * x
+    // enforces x^3 = x^2 * x
     // A: [(1, 3)] => 1*z[3] = 1*9 = 9
     // B: [(1, 2)] => 1*z[2] = 1*3 = 3
     // C: [(1, 4)] => 1*z[4] = 1*27 = 27
 
-    // Enforces x^3 + x + 5 - y = 0
+    // enforces x^3 + x + 5 - y = 0
     // A: [(5, 0), (-1, 1), (1, 2), (1, 4)] => 5*1 + (-1)*35 + 1*3 + 1*27 = 5 - 35 + 3 + 27 = 0
     // B: [(1, 0)] => 1*z[0] = 1*1 = 1
     // C: [] => 0
+
+    // setup
+    let mut rng = thread_rng();
+    let (pk, vk): (ProvingKey<Bls12_381>, VerifyingKey<Bls12_381>) = 
+        Groth16::<Bls12_381>::circuit_specific_setup(circuit.clone(), &mut rng).unwrap();
+
+    // prove
+    let prove_start = Instant::now();
+    let proof: Proof<Bls12_381> = Groth16::<Bls12_381>::prove(&pk, circuit.clone(), &mut rng).unwrap();
+    let prove_time = prove_start.elapsed();
+    println!("\nProof generated successfully in {:?}", prove_time);
+
+    // verify
+    let verify_start = Instant::now();
+    let valid = Groth16::<Bls12_381>::verify(&vk, &[y_value], &proof).unwrap();
+    let verify_time = verify_start.elapsed();
+    println!("Proof verification result: {} in {:?}", valid, verify_time);
 
 }
